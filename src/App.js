685876e6eb997db318ae7fd559ca0b19bb3fba1c
xokoe78 <69820866+xokoe78@users.noreply.github.com>
2022-10-02 17:49:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component }  from 'react';
-import Map, { GeolocateControl } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function MyMap() {
-  return (
-    <div>
-      <Map
-        mapboxAccessToken="MAPBOX_TOKEN"
-        initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5,
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-      >
-        <GeolocateControl
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-      </Map>
-    </div>
-  );
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
+
+  render() {
+    const { loading, msg } = this.state;
+
+    return (
+      <p>
+        <button onClick={this.handleClick}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    );
+  }
 }
-export default MyMap;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <LambdaDemo />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
