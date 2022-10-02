677d69441c40668891f421cb5f07273d16e4f250
xokoe78 <69820866+xokoe78@users.noreply.github.com>
2022-10-02 18:47:30 +0100

    changes

--- src/Map.js
+++ src/Map.js
@@ -4,28 +4,6 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken='pk.eyJ1IjoieG9rb2UiLCJhIjoiY2w4cmk1cWVqMW1sejN1bzVsaW80ZXV2YiJ9.sNukazh3PYPUy5qHak9hRA';
 
-// Sample data 
-const data = [
-	{
-		"location": "Manhattan Ave & Norman Ave at NE corner",
-		"city": "Brooklyn",
-		"state": "New York",
-		"coordinates": [-73.9516030004786,40.72557300071668],
-	},
-	{
-		"location": "6th Ave & 42nd St at NW corner",
-		"city": "Manhattan",
-		"state": "New York",
-		"coordinates": [-73.98393399979334,40.75533300052329],
-	},
-	{
-		"location": "Essex St & Delancey St at SE corner",
-		"city": "Manhattan",
-		"state": "New York",
-		"coordinates": [-73.9882730001973,40.718207001246554],
-	}
-]
-
 class Mapp extends React.Component{
 
 	// Set up states for updating map 
@@ -47,15 +25,6 @@ class Mapp extends React.Component{
 			zoom: this.state.zoom
 		})
 
-		data.forEach((location) => {
-			console.log(location)
-			var marker = new mapboxgl.Marker()
-							.setLngLat(location.coordinates)
-							.setPopup(new mapboxgl.Popup({ offset: 30 })
-							.setHTML('<h4>' + location.city + '</h4>' + location.location))
-							.addTo(map);
-
-		})
 	}
 
 	render(){
